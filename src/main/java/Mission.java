d9f39b43d1b76237d1bde0e7447f86010cf5ae49
annalanigan <annalanigan@outlook.com>
2018-01-17 12:08:19 +0000

    changed tack - moved the data from the Mission to the Test and adding a check for scent

--- src/main/java/Mission.java
+++ src/main/java/Mission.java
@@ -3,35 +3,31 @@ import java.util.ArrayList;
 public class Mission {
 
      private Mars myPlanet;
-     private Robot robot1;
-     private Robot robot2;
-     private Robot robot3;
-     private String instructions1;
-     private String instructions2;
-     private String instructions3;
      private ArrayList<String> lostCoords;
 
-    public Mission(){
-        myPlanet = new Mars(5, 3);
-        robot1 = new Robot(1, 1, "E");
-        robot2 = new Robot(3, 2, "N");
-        robot3 = new Robot(0, 3, "W");
-        instructions1 = "RFRFRFRF";
-        instructions2 = "FRRFLLFFRRFLL";
-        instructions3 = "LLFFFLFLFL";
-        lostCoords = new ArrayList<>();
+    public Mission(int x, int y){
+        myPlanet = new Mars(x, y);
+        lostCoords = new ArrayList<String>();
     }
 
-    public String robot1Mission(){
-        return robot1.receiveInstructionList(instructions1);
+    public Mars getMyPlanet() {
+        return myPlanet;
     }
 
-    public String robot2Mission(){
-        return robot2.receiveInstructionList(instructions2);
+    public void setMyPlanet(Mars myPlanet) {
+        this.myPlanet = myPlanet;
     }
 
-    public String robot3Mission(){
-        return robot3.receiveInstructionList(instructions3);
+    public ArrayList<String> getLostCoords() {
+        return lostCoords;
+    }
+
+    public void setLostCoords(ArrayList<String> lostCoords) {
+        this.lostCoords = lostCoords;
+    }
+
+    public void addToLostCoords(String data){
+        lostCoords.add(data);
     }
 
     //public String or Boolean lostRobot(Robot robot){
@@ -40,7 +36,25 @@ public class Mission {
     // Return String of coodrs + " LOST"
     //}
 
+     public boolean checkForScent(String currentCoords) {
+        if(lostCoords.size() > 0){
+            for(String single : lostCoords){
+                if (single.equals(currentCoords)){
+                  return true;
+                }
+            }
+        }
+        return false;
+    }
 
-
+    public boolean hasFallenOffEdge ( int xCoord, int yCoord){
+        if (xCoord > myPlanet.getSizeOfInner()) {
+            return true;
+        } else if (yCoord > myPlanet.getSizeOfOuter()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
--- src/test/java/MissionTest.java
+++ src/test/java/MissionTest.java
@@ -6,25 +6,28 @@ import static org.junit.Assert.assertEquals;
 public class MissionTest {
 
     private Mission myMission;
+    private Robot robot1;
+    private Robot robot2;
+    private Robot robot3;
+    private String instructions1;
+    private String instructions2;
+    private String instructions3;
 
     @Before
     public void before(){
-        myMission = new Mission();
+        myMission = new Mission(5, 3);
+        robot1 = new Robot(1, 1, "E");
+        robot2 = new Robot(3, 2, "N");
+        robot3 = new Robot(0, 3, "W");
+        instructions1 = "RFRFRFRF";
+        instructions2 = "FRRFLLFFRRFLL";
+        instructions3 = "LLFFFLFLFL";
     }
 
     @Test
-    public void robot1Test(){
-        assertEquals("1 1 E", myMission.robot1Mission());
-    }
-
-    @Test
-    public void robot2Test(){
-        assertEquals("3 3 N LOST", myMission.robot2Mission());
-    }
-
-    @Test
-    public void robot3Test(){
-        assertEquals("2 3 S", myMission.robot3Mission());
+    public void canCheckForScent(){
+        myMission.addToLostCoords("5 5 N");
+        assertEquals(true, myMission.checkForScent("5 5 N"));
     }
 
 
